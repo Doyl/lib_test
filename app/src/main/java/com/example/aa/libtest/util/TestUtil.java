a3d71964cb839d1b423d052f987fad3ca25f86b9
duyongliang <duyongliang@aerozhonghuan.comgit initgit config --global user.email duyongliang@aerozhonghuan.com>
2018-12-24 16:35:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aa/libtest/util/TestUtil.java
@@ -0,0 +1,11 @@
+package com.example.aa.libtest.util;
+
+/**
+ * Created by duyongliang
+ * on 2018/12/24.
+ */
+public class TestUtil {
+    public static String getLibMsg(){
+        return "LibMsg";
+    }
+}
